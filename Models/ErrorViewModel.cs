de223c23b3dd2936cb537c2ae2b0ed37602d182f
yahyagulshan <59036269+yahyagulshan@users.noreply.github.com>
2022-11-18 00:50:15 +0500

    Create ErrorViewModel.cs

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace azure_pipeline_test.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
